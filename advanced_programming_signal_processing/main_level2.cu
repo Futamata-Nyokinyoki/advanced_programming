3115adae8e48087d1595b0c3e10de440d964f2be
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-06-04 22:52:30 +0900

    fix: シェルスクリプトからC言語に置き換え(level2用)

--- advanced_programming_signal_processing/main_level2.cu
+++ advanced_programming_signal_processing/main_level2.cu
@@ -1,8 +1,11 @@
-#include "imageUtil.h"
+#include "imageUtil.cuh"
+
+#include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <glob.h>
 // #include <omp.h>
 
 void templateMatchingGray(Image *src, Image *templ, Point *position, double *distance)
@@ -117,30 +120,11 @@ int isMatchColor(Image *src, Image *templ, int x, int y)
     return 1;
 }
 
-// test/beach3.ppm template /airgun_women_syufu.ppm 0 0.5 cwp
-int main(int argc, char **argv)
+int level2(char *input_file, char *templ_file, int rotation, double threshold, char *options)
 {
-    if (argc < 5)
-    {
-        fprintf(stderr, "Usage: templateMatching src_image temlate_image rotation threshold option(c,w,p,g)\n");
-        fprintf(stderr, "Option:\nc) clear a txt result. \nw) write result a image with rectangle.\np) print results.\n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0  \n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0 c\n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0 w\n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0 p\n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0 g\n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0 cw\n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0 cwp\n");
-        fprintf(stderr, "ex: templateMatching src_image.ppm temlate_image.ppm 0 1.0 cwpg\n");
-        return -1;
-    }
-
-    char *input_file = argv[1];
-    char *template_file = argv[2];
-    int rotation = atoi(argv[3]);
-    double threshold = atof(argv[4]);
-
-    printf("rotation -> %d\n", rotation);
+    CalcTime t;
+    // 初期化
+    t.start();
 
     char output_name_base[256];
     char output_name_txt[256];
@@ -155,58 +139,67 @@ int main(int argc, char **argv)
     int isPrintResult = 0;
     int isGray = 0;
 
-    if (argc == 6)
+    if (options != NULL)
     {
         char *p = NULL;
-        if (p = strchr(argv[5], 'c') != NULL)
+        if ((p = strchr(options, 'c')) != NULL)
             clearResult(output_name_txt);
-        if (p = strchr(argv[5], 'w') != NULL)
+        if ((p = strchr(options, 'w')) != NULL)
             isWriteImageResult = 1;
-        if (p = strchr(argv[5], 'p') != NULL)
+        if ((p = strchr(options, 'p')) != NULL)
             isPrintResult = 1;
-        if (p = strchr(argv[5], 'g') != NULL)
+        if ((p = strchr(options, 'g')) != NULL)
             isGray = 1;
     }
 
     Image *img = readPXM(input_file);
-    Image *template = readPXM(template_file);
+    Image *templ = readPXM(templ_file);
 
     Point result;
     double distance = 0.0;
+    // 初期化終了
+    t.end();
+    printf("画像：%s\n", input_file);
+    printf("テンプレートファイル：%s\n", templ_file);
+    printf("初期化. %5.2lf[ms]\n", t.getAvgTime(false));
 
+    // テンプレートマッチング開始
+    t.start();
     if (isGray && img->channel == 3)
     {
         Image *img_gray = createImage(img->width, img->height, 1);
-        Image *template_gray = createImage(template->width, template->height, 1);
+        Image *templ_gray = createImage(templ->width, templ->height, 1);
         cvtColorGray(img, img_gray);
-        cvtColorGray(template, template_gray);
+        cvtColorGray(templ, templ_gray);
 
-        templateMatchingGray(img_gray, template_gray, &result, &distance);
-
-        freeImage(img_gray);
-        freeImage(template_gray);
+        templateMatchingGray(img_gray, templ_gray, &result, &distance);
     }
     else
     {
-        templateMatchingColor(img, template, &result, &distance);
+        templateMatchingColor(img, templ, &result, &distance);
     }
+    // テンプレートマッチング終了
+    t.end();
+    printf("メイン. %5.2lf[ms]\n", t.getAvgTime(false));
 
+    // 後処理開始
+    t.start();
     if (distance < threshold)
     {
-        writeResult(output_name_txt, getBaseName(template_file), result, template->width, template->height, rotation, distance);
+        writeResult(output_name_txt, getBaseName(templ_file), result, templ->width, templ->height, rotation, distance);
         if (isPrintResult)
         {
-            printf("[Found    ] %s %d %d %d %d %d %f\n", getBaseName(template_file), result.x, result.y, template->width, template->height, rotation, distance);
+            // printf("[Found    ] %s %d %d %d %d %d %f\n", getBaseName(templ_file), result.x, result.y, templ->width, templ->height, rotation, distance);
         }
         if (isWriteImageResult)
         {
-            drawRectangle(img, result, template->width, template->height);
+            drawRectangle(img, result, templ->width, templ->height);
 
             if (img->channel == 3)
                 strcat(output_name_img, ".ppm");
             else if (img->channel == 1)
                 strcat(output_name_img, ".pgm");
-            printf("out: %s", output_name_img);
+            // printf("out: %s", output_name_img);
             writePXM(output_name_img, img);
         }
     }
@@ -214,12 +207,73 @@ int main(int argc, char **argv)
     {
         if (isPrintResult)
         {
-            printf("[Not found] %s %d %d %d %d %d %f\n", getBaseName(template_file), result.x, result.y, template->width, template->height, rotation, distance);
+            // printf("[Not found] %s %d %d %d %d %d %f\n", getBaseName(templ_file), result.x, result.y, templ->width, templ->height, rotation, distance);
         }
     }
 
     freeImage(img);
-    freeImage(template);
+    freeImage(templ);
 
+    // 後処理終了
+    t.end();
+    printf("後処理. %5.2lf[ms]\n", t.getAvgTime(false));
+
+    printf("\n");
+
+    return 0;
+}
+
+void process_image(char *image, char *level)
+{
+    char *bname = getBaseName(image);
+    strcat(bname, ".ppm");
+    char *name = (char *)malloc(256);
+    strcpy(name, "imgproc/");
+    strcat(name, bname);
+    printf("name: %s\n", name);
+
+    int rotation = 0;
+
+    glob_t glob_result;
+    char *search_path = (char *)malloc(256);
+    strcpy(search_path, level);
+    strcat(search_path, "/*.ppm");
+    glob(search_path, GLOB_TILDE, NULL, &glob_result);
+
+    int x = 0;
+    for (unsigned int i = 0; i < glob_result.gl_pathc; i++)
+    {
+        char *templ = glob_result.gl_pathv[i];
+        if (x == 0)
+        {
+            level2(name, templ, rotation, 0.5, "cwp");
+            x = 1;
+        }
+        else
+        {
+            level2(name, templ, rotation, 0.5, "wp");
+        }
+    }
+    globfree(&glob_result);
+}
+
+int main(int argc, char *argv[])
+{
+    char *level = argv[1];
+    glob_t glob_result;
+
+    char input_path[256];
+    strcpy(input_path, level);
+    strcat(input_path, "/final/*.ppm");
+    printf("test file path: %s\n", input_path);
+
+    glob(input_path, GLOB_TILDE, NULL, &glob_result);
+    for (unsigned int i = 0; i < glob_result.gl_pathc; ++i)
+    {
+        char *image = glob_result.gl_pathv[i];
+        printf("image: %s\n", image);
+        process_image(image, level);
+    }
+    globfree(&glob_result);
     return 0;
 }
